e9797c632a9022a935be9df3789937565941afd3
Eric Suescun <edsuescun@gmail.com>
2019-07-09 09:17:36 -0500

    Create action added to the controller with codes for success and errors.

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -7,6 +7,30 @@ module Api
         respond_with Product.all
       end
 
+      # def create
+      #   user = User.new(user_params)
+      #   if user.save
+      #     render json: user, status: 201, location: [:api, user]
+      #   else
+      #     render json: { errors: user.errors }, status: 422
+      #   end
+      # end
+
+      def create
+        product = Product.new(product_params)
+        if product.save
+          render json: product, status: 201, location: [:api, product]
+        else
+          render json: { errors: product.errors }, status: 422
+        end
+      end
+
+      private
+
+        def product_params
+          params.require(:product).permit(:name, :price)
+        end
+
     end
   end
 end
